28391b1ab4ce33aa6cadf40bd80bc1b7ae74ef44
knilch0r <knilch0r@users.noreply.github.com>
2021-03-31 20:57:08 +0200

    add device infrastructure.

--- knilchroot.c
+++ knilchroot.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/device.h>
 #include <asm/uaccess.h>
 
 MODULE_LICENSE("GPL");
@@ -13,6 +14,7 @@ MODULE_VERSION("0.0");
 static ssize_t knilchroot_read(struct file *, char *, size_t, loff_t *);
 
 static int major_num;
+static struct class * knilchroot_class;
 
 static struct file_operations file_ops = {
 	.owner = THIS_MODULE,
@@ -31,18 +33,30 @@ static ssize_t knilchroot_read(struct file *f, __user char *b, size_t l, loff_t 
 }
 
 static int __init knilchroot_init(void) {
+
 	major_num = register_chrdev(0, "knilchroot", &file_ops);
 	if (major_num < 0) {
 		printk(KERN_ALERT "Could not register knilchroot: %d\n", major_num);
 		return major_num;
-	} else {
-		printk(KERN_INFO "knilchroot loaded, major %d\n", major_num);
-		return 0;
 	}
+
+	knilchroot_class = class_create(THIS_MODULE, "knilchroot");
+	if (IS_ERR(knilchroot_class)) {
+		unregister_chrdev(major_num, "knilchroot");
+		return PTR_ERR(knilchroot_class);
+	}
+
+	device_create(knilchroot_class, NULL, MKDEV(major_num, 0), NULL, "knilchroot%d", 0);
+
+	printk(KERN_INFO "knilchroot loaded, major %d\n", major_num);
+	return 0;
 }
 
 static void __exit knilchroot_exit(void) {
+	device_destroy(knilchroot_class, MKDEV(major_num, 0));
+	class_destroy(knilchroot_class);
 	unregister_chrdev(major_num, "knilchroot");
+
 	printk(KERN_INFO "knilchroot unloaded\n");
 }
 
